73cee397e695392fd9cc0eac83f565f87908b566
Mason Chinkin <mason.chinkin@gmail.com>
2019-01-22 22:41:12 -0800

    Finish w3d3 hw

---
+++ w3d3/demo_project/app/models/person.rb
@@ -0,0 +1,13 @@
+# create_table "people", force: :cascade do |t|
+#   t.string "name", null: false
+#   t.integer "house_id", null: false
+# end
+
+class Person < ActiveRecord::Base
+  validates :name, presence: true
+
+  belongs_to :house,
+    primary_key: :id,
+    foreign_key: :house_id,
+    class_name: :House
+end
